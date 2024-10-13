ad24f63d351b85ab447b513830f8e59cece4bd33
yousifmedhat98 <y.medhat@bimventures.com>
2024-10-13 10:26:32 +0300

    Adding improvements

---
+++ app/(public)/login/page.js
@@ -0,0 +1,24 @@
+"use client";
+
+import React from "react";
+import LoginForm from "../../../components/login-form";
+import { useEffect } from "react";
+import Cookies from "js-cookie";
+
+const Login = () => {
+  useEffect(() => {
+    if (Cookies.get("authToken")) {
+      console.log("Auth token is set");
+    } else {
+      console.log("No auth token ");
+    }
+  }, []);
+  
+  return (
+    <div>
+      <LoginForm />
+    </div>
+  );
+};
+
+export default Login;
--- components/login-form.js
+++ components/login-form.js
@@ -1,41 +1,41 @@
-'use client'
+"use client";
 import InputController from "./input-controller";
 import { useForm } from "react-hook-form";
-import { authenticateProtected } from '../Auth/auth';
+import { authenticateProtected } from "../Auth/auth";
 
 const LoginForm = () => {
-  const { control, handleSubmit, reset } = useForm();
-  
+  const { control, handleSubmit } = useForm();
+
   const onSubmit = async (data) => {
     const { email, password } = data;
     const result = await authenticateProtected(null, email, password);
-  
-    console.log(result)
+
+    console.log(result);
   };
+
   return (
     <form
-    className="bg-orange-500 p-5 space-y-5 container mx-auto max-w-lg"
-    onSubmit={handleSubmit(onSubmit)}
+      className="bg-orange-500 p-5 space-y-5 container mx-auto max-w-lg"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <p className="text-4xl">Login</p>
+      <InputController
+        control={control}
+        placeholder="Enter your name"
+        type="email"
+        name="username"
+      />
+      <InputController
+        control={control}
+        placeholder="Enter your password"
+        type="password"
+        name="password"
+      />
+      <button className="border border-white rounded-sm w-full py-2.5 text-white font-semibold hover:bg-white hover:text-orange-500">
+        Login
+      </button>
+    </form>
+  );
+};
 
-  >
-    <p className="text-4xl">Login</p>
-    <InputController
-      control={control}
-      placeholder="Enter your name"
-      type="email"
-      name="username"
-    />
-    <InputController
-      control={control}
-      placeholder="Enter your password"
-       type="password"
-      name="password"
-    />
-    <button className="border border-white rounded-sm w-full py-2.5 text-white font-semibold hover:bg-white hover:text-orange-500">
-      Login
-    </button>
-  </form>
-  )
-}
-
-export default LoginForm
+export default LoginForm;
